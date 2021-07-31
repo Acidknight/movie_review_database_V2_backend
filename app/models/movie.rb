8342a2945aa80c8c64738306e4b60b4029404cb2
Anthony Leon <Acidknights89@gmail.com>
2021-07-30 21:19:21 -0400

    Add dependency

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
     belongs_to :genre
 
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
 
     validates :title, :release_year, :description, :image_url, :starring_actors, :genre_id, presence: true
 end
