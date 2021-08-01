966b9b8e2e9a2c0170970cc31532625db0065dfd
Anthony Leon <Acidknights89@gmail.com>
2021-08-01 03:12:18 -0400

    Fixing typos

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,4 @@
 class Movie < ApplicationRecord
     belongs_to :genre
-
     validates :title, :release_year, :description, :image_url, :starring_actors, :genre_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,5 @@ mystery = Genre.create(name: "Mystery")
 suspense = Genre.create(name: "Suspense")
 
 
+
 Movie.create(title: "The Mask", release_year: "1996", description: "Funny stuff", image_url: "https://images.moviesanywhere.com/99f8f20a1a1d5517c71ac7735484fff8/ec5eb97a-54a7-4615-b773-b976d2b36a90.jpg", starring_actors: "Jim Carrey, Cameron Diaz", genre_id: comedy.id)
